fd7b6d8658005cc1a5e162f99d6d32b0a3aff142
SSARIpa3 <swati.0491@gmail.com>
2021-08-17 09:20:09 +0530

    Adding navigation with React router DOM

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './App.css';
-import Homepage from './Pages/homepage/homepage.component';
- 
+import React from "react";
+import "./App.css";
+import Homepage from "./Pages/homepage/homepage.component";
+import { Route, Link } from "react-router-dom";
+import HomePage from "./Pages/homepage/homepage.component";
+
+export const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATSPAGE</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-        <Homepage/>      
+      <Route exact path="/" component={Homepage} />
+      <Route exact path="/hats" component={HatsPage} />
     </div>
   );
 }
