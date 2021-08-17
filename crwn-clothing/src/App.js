268e3a94dfa11fc66ee768ed8264ed38e2fe5194
SSARIpa3 <swati.0491@gmail.com>
2021-08-17 12:21:21 +0530

    Adding Headers for our Application

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./Pages/homepage/homepage.component";
-import { Route, Link } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 import HomePage from "./Pages/homepage/homepage.component";
 import ShopPage from "./Pages/shop/shop.component";
+import Header from "./Components/header/header.component";
 
 function App() {
   return (
     <div>
-      <Route exact path="/" component={Homepage} />
-      <Route exact path="/shop" component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
