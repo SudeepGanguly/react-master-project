8e1460a9f6f4ed095cb1dbf60d727398758a74a3
SSARIpa3 <swati.0491@gmail.com>
2021-08-25 08:28:20 +0530

    added additem and removeItem funcctionality to checkoutyy item component

--- crwn-clothing/src/redux/cart/cart.utils.js
+++ crwn-clothing/src/redux/cart/cart.utils.js
@@ -12,3 +12,23 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   }
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+  if (existingCartItem) {
+    //If quantity is only one , then remove the item itself
+    if (existingCartItem.quantity === 1) {
+      return cartItems.filter(
+        (cartItem) => cartItem.id !== existingCartItem.id
+      );
+    }
+
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToRemove.id
+        ? { ...cartItem, quantity: cartItem.quantity - 1 }
+        : cartItem
+    );
+  }
+};
